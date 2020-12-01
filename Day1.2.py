b6a24286fdd05e61236b7e6d78c0bab82ce2e823
Joren Guillaume <guillaume.joren@gmail.com>
2020-12-01 08:41:44 +0100

    Day1.2

---
+++ Day1.2.py
@@ -0,0 +1,32 @@
+filepath = 'input1.1'
+som = 0
+
+with open(filepath) as fp:
+    line = fp.readline()
+    linelist = []
+    listlength = 0
+    while line:
+        linelist.append(int(line))
+        line = fp.readline()
+        listlength += 1
+    linelist.sort()
+
+    for x in range(0, listlength):
+        found = False
+        x2 = x + 1
+        x3 = x2 + 1
+        num1 = linelist[x]
+        elesum = linelist[x] + linelist[x2] + linelist[x3]
+        while elesum < 2020 and not found:
+            x2 += 1
+            x3 += x2 + 1
+            elesum = linelist[x] + linelist[x2] + linelist[x3]
+            while elesum < 2020 and not found:
+                x3 += 1
+                elesum = linelist[x] + linelist[x2] + linelist[x3]
+            if elesum == 2020:
+                eleproduct = linelist[x] * linelist[x2] * linelist[x3]
+                found = True
+                print(eleproduct)
+        if found:
+            break
