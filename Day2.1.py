89b9721e999914c4ddd5566ae119f887c313ea8e
Mraedis <>
2020-12-02 10:41:57 +0100

    Day 2

---
+++ Day2.1.py
@@ -0,0 +1,14 @@
+filepath = 'input2.1'
+
+validpass = 0
+
+with open(filepath) as fp:
+    line = fp.readline()
+    while line:
+        policy, passkey, password = line.split(' ')
+        numkey = password.count(passkey[0])
+        lowpol, uppol = policy.split('-')
+        if int(lowpol) <= numkey <= int(uppol):
+            validpass += 1
+        line = fp.readline()
+    print(validpass)
---
+++ Day2.2.py
@@ -0,0 +1,13 @@
+filepath = 'input2.1'
+
+validpass = 0
+
+with open(filepath) as fp:
+    line = fp.readline()
+    while line:
+        policy, passkey, password = line.split(' ')
+        lowpol, uppol = policy.split('-')
+        if (password[int(lowpol)-1] == passkey[0]) != (password[int(uppol)-1] == passkey[0]):
+            validpass += 1
+        line = fp.readline()
+    print(validpass)
