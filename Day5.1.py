15c88810ce988df5c240c2520658b545bd97eb40
Mraedis <>
2020-12-05 09:48:56 +0100

    Day5

---
+++ Day5.1.py
@@ -0,0 +1,19 @@
+filepath = 'input5'
+
+
+def bin_to_dec(num):
+    dec = 0
+    for x in num:
+        dec = dec * 2 + int(x)
+    return dec
+
+
+seatlist = []
+
+with open(filepath) as fp:
+    line = fp.readline()
+    while line:
+        line = line.strip().replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
+        seatlist.append(bin_to_dec(line))
+        line = fp.readline()
+print(sorted(seatlist)[-1])
