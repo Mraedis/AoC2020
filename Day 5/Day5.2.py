56e5d010726d59dfa0d593d9bcab88999ea1eaa8
Mraedis <chaos.side@gmail.com>
2020-12-07 16:41:25 +0100

    Fixed day 5 stupid

--- Day 5/Day5.2.py
+++ Day 5/Day5.2.py
@@ -1,23 +1,17 @@
 filepath = 'input5'
 
-
-def bin_to_dec(num):
-    dec = 0
-    for n in num:
-        dec = dec * 2 + int(n)
-    return dec
-
-
 seatlist = []
 with open(filepath) as fp:
     line = fp.readline()
     while line:
         line = line.strip().replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
-        seatlist.append(bin_to_dec(line))
+        seatlist.append(int(line, 2))
         line = fp.readline()
 
 seatlist = sorted(seatlist)
+seat = 0
 
 for x in range(1, len(seatlist)-1):
     if ((seatlist[x] + seatlist[x+1] + seatlist[x-1]) / 3) % 1:
-        print(seatlist[x])
+        seat += seatlist[x]
+print(seat/2)
