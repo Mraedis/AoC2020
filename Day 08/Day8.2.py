2dea1acb8db77cb6f88ec630d5d3fab3c9e140cc
Mraedis <chaos.side@gmail.com>
2020-12-11 19:46:14 +0100

    Fixed day 8.2 that worked despite bug

--- Day 08/Day8.2.py
+++ Day 08/Day8.2.py
@@ -2,16 +2,17 @@ oplist = [[op for op in line.strip().split(' ')] for line in open('input8').read
 change = 0
 
 for x in range(0, len(oplist)):
-    oplist[x][0] = 'jmp' if oplist[x][0] == 'nop' else 'jmp'
-    acc = 0
-    run = []
-    lastrun = 0
-    while lastrun not in run and lastrun < len(oplist):
-        run.append(lastrun)
-        op = oplist[lastrun]
-        acc += (op[0] == 'acc') * int(op[1])
-        lastrun += (op[0] == 'nop') + (op[0] == 'acc') + (op[0] == 'jmp') * int(op[1])
-    if lastrun == len(oplist):
-        change = acc
-        oplist[x][0] = 'jmp' if oplist[x][0] == 'nop' else 'jmp'
+    if oplist[x][0] in ['jmp', 'nop']:
+        oplist[x][0] = 'jmp' if oplist[x][0] == 'nop' else 'nop'
+        acc = 0
+        run = []
+        lastrun = 0
+        while lastrun not in run and lastrun < len(oplist):
+            run.append(lastrun)
+            op = oplist[lastrun]
+            acc += (op[0] == 'acc') * int(op[1])
+            lastrun += (op[0] == 'nop') + (op[0] == 'acc') + (op[0] == 'jmp') * int(op[1])
+        if lastrun == len(oplist):
+            change = acc
+        oplist[x][0] = 'jmp' if oplist[x][0] == 'nop' else 'nop'
 print(change)
