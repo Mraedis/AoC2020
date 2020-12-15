f35779436ff04a2b766302f91ff8b7e7539b988e
Mraedis <chaos.side@gmail.com>
2020-12-15 11:29:25 +0100

    Day 15 small rewrite

--- Day 15/Day15.2.py
+++ Day 15/Day15.2.py
@@ -1,11 +1,9 @@
 inp = [0, 14, 1, 3, 7, 9]
 
-turns = {}
-for x, num in enumerate(inp):
-    turns[num] = x
+turns = dict((num, x) for x, num in enumerate(inp))
 last = inp[-1]
 
-for i in range(len(turns.keys()), 30000000):
+for i in range(len(turns.keys()), 2020):
     nnum = 0 if last not in turns else i - 1 - turns[last]
     turns[last] = i - 1
     last = nnum
